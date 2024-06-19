d5bc76e50123488787dc3543d29d963f88364d7f
Nikhel MN <mnnikhel9@gmail.com>
2024-06-19 14:05:52 +0530

    Update main.tf

--- sentry-terraform/main.tf
+++ sentry-terraform/main.tf
@@ -1,50 +1,18 @@
-resource "aws_key_pair" "sentry_key_pair" {
-  key_name   = var.key_name
-  public_key = tls_private_key.ssh_key.public_key_openssh
+provider "aws" {
+  region = "ap-south-1"
 }
 
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_instance" "sentry" {
-  ami                    = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (change as needed)
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.sentry_key_pair.key_name
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+resource "aws_instance" "sentry_vm" {
+  ami           = "ami-0167b3c8929118d4d" # Amazon Linux 2 AMI
+  instance_type = "t2.medium"
 
   tags = {
-    Name = "SentryServer"
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name_prefix = "sentry-ssh-"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    Name = "Sentry-VM"
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  key_name      = "mumbai"
 }
 
 output "instance_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.sentry.public_ip
-}
-
-output "private_key_pem" {
-  description = "The private key in PEM format"
-  value       = tls_private_key.ssh_key.private_key_pem
-  sensitive   = true
+  value = aws_instance.sentry_vm.public_ip
 }
