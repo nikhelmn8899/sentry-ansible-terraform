3801340607beda6a41f34d5f9618fb9ed50877f1
Nikhel MN <mnnikhel9@gmail.com>
2024-06-19 14:07:03 +0530

    Update variable.tf

--- sentry-terraform/variable.tf
+++ sentry-terraform/variable.tf
@@ -1,14 +1,6 @@
-variable "region" {
-  description = "The AWS region to deploy the resources."
-  default     = "us-west-2"
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+  default     = "ap-south-1"
 }
 
-variable "instance_type" {
-  description = "The type of instance to deploy."
-  default     = "t2.micro"
-}
-
-variable "key_name" {
-  description = "The name of the key pair to create and use for the instance."
-  default     = "sentry-key-pair"
-}
